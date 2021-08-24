0d747a82cffd14e9acf83bef22666e1ed26fe640
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-08-24 00:16:57 -0400

    login form constructed

--- potluckplanner-frontend/src/components/Login.js
+++ potluckplanner-frontend/src/components/Login.js
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
-    return null
+    const [loginValues, setLoginValues] = useState({
+        username: "",
+        password: ""
+    })
+
+    const update = event => {
+        setLoginValues({ 
+            ...loginValues, 
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    const submit = event => {
+        event.preventDefault()
+        console.log(loginValues)
+        setLoginValues({
+            username: "",
+            password: ""
+        })
+    }
+
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            {console.log(loginValues)}
+            <form className="login-form" onSubmit={event => submit(event)}>
+                <label>
+                    Username:
+                    <input 
+                        type="text"
+                        name="username"
+                        value={loginValues.username}
+                        onChange={update}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input 
+                        type="password"
+                        name="password"
+                        value={loginValues.password}
+                        onChange={update}
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
